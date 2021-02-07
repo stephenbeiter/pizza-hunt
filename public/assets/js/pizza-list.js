37fb99ca8df86acb2862d4189da65e09f0436f6c
Stephen Beiter <stephenbeiter@gmail.com>
2021-02-06 22:20:04 -0800

    added comment functionality

--- public/assets/js/pizza-list.js
+++ public/assets/js/pizza-list.js
@@ -1,5 +1,16 @@
 const $pizzaList = document.querySelector('#pizza-list');
 
+const getPizzaList = () => {
+  fetch('/api/pizzas')
+    .then(response => response.json())
+    .then(pizzaListArr => {
+      pizzaListArr.forEach(printPizza);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
 const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, createdAt }) => {
   const pizzaCard = `
     <div class="col-12 col-lg-6 flex-row">
@@ -8,15 +19,15 @@ const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, c
         <div class="card-body flex-column col-auto">
           <h4 class="text-dark">By ${createdBy}</h4>
           <p>On ${createdAt}</p>
-          <p>${commentCount} Comments</p>
+          <p>${commentCount} Comment(s)</p>
           <h5 class="text-dark">Suggested Size: ${size}
           <h5 class="text-dark">Toppings</h5>
           <ul>
             ${toppings
-              .map(topping => {
-                return `<li>${topping}</li>`;
-              })
-              .join('')}
+      .map(topping => {
+        return `<li>${topping}</li>`;
+      })
+      .join('')}
           </ul>
           <a class="btn display-block w-100 mt-auto" href="/pizza?id=${_id}">See the discussion.</a>
         </div>
@@ -26,3 +37,5 @@ const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, c
 
   $pizzaList.innerHTML += pizzaCard;
 };
+
+getPizzaList();
